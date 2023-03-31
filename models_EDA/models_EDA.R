f385b40d09ad6a9142388cb1c2efbb3f6d549032
Gabriel Alvaro <gabriel.batista.1801@gmail.com>
2023-03-31 15:49:34 -0300

    Update models_EDA.R

--- models_EDA/models_EDA.R
+++ models_EDA/models_EDA.R
@@ -14,37 +14,55 @@ library(tidyverse)
 #                 `T (degC)`,
 #                 `rh (%)`)
 # 
-# data = data %>%
+# # daily
+# data_daily = data %>%
+#   mutate(Date = format(as.Date(dmy_hms(`Date Time`)), "%Y-%m-%d")) %>%
+#   group_by(Date) %>%
+#   summarise(Temperature = mean(`T (degC)`),
+#             RelativeHumidity = mean(`rh (%)`))
+# 
+# # monthly 
+# data_monthly = data %>%
 #   mutate(Date = format(as.Date(dmy_hms(`Date Time`)), "%Y-%m")) %>%
 #   group_by(Date) %>%
 #   summarise(Temperature = mean(`T (degC)`),
 #             RelativeHumidity = mean(`rh (%)`))
 # 
-# readr::write_csv(as.data.frame(data), file = "max_planck_weather_monthly.csv")
+# readr::write_csv(as.data.frame(data_daily), file = "max_planck_weather_daily.csv")
+# readr::write_csv(as.data.frame(data_monthly), file = "max_planck_weather_monthly.csv")
 
 # importando dados
-data = read_csv("https://raw.githubusercontent.com/gabriel-alvaro/ME607-1S2023/main/models_EDA/max_planck_weather_monthly.csv") %>%
-  mutate(Date = yearmonth(Date)) %>%
-  as_tsibble()
+weather = read_csv("https://raw.githubusercontent.com/gabriel-alvaro/ME607-1S2023/main/models_EDA/max_planck_weather_daily.csv")
+weather = as_tsibble(weather)
 
-autoplot(data, .vars = Temperature) +
+autoplot(weather, .vars = Temperature) +
   xlab("Date") +
   ylab("Temperature (Celsius)") +
+  theme_bw() +
   scale_y_continuous(n.breaks = 6) +
   scale_x_yearmonth(date_breaks = "1 year",
-                    date_label = "%Y-%m") +
-  theme_bw()
+                    date_label = "%d-%m-%Y") +
+  theme(axis.text.x = element_text(angle = 30, hjust=1))
 
 ## MODELOS
 ## media, naive, naive sazonal, drift, regressao linear, suavizacao exponencial
 
-data_train = data |>
-  stretch_tsibble(.step = 1, .init = 100)
+weather_train_rw = weather |>
+  tile_tsibble(.size = 50)
 
-data_train |>
-  model(RW(Temperature ~ drift())) |>
+weather_train_sw = weather |>
+  stretch_tsibble(.step = 1, .init = 50)
+
+weather_train_rw |>
+  model(mean = MEAN(Temperature)) |>
   forecast(h = 1) |>
-  accuracy(data)
-  
+  accuracy(weather)
+
+weather_train_sw |>
+  model(mean = MEAN(Temperature)) |>
+  forecast(h = 1) |>
+  accuracy(weather)
+
+
 
 
