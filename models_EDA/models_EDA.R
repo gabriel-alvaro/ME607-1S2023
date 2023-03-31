2500e7dc2143b9ec86aa1a6801b5a8dce2f464fb
Gabriel Alvaro <gabriel.batista.1801@gmail.com>
2023-03-31 19:24:32 -0300

    Update datasets
    Filter datasets to 2014-2016

--- models_EDA/models_EDA.R
+++ models_EDA/models_EDA.R
@@ -10,34 +10,46 @@ library(tidyverse)
 
 # data = readr::read_csv("max_planck_weather_ts.csv")
 # 
-# data = data %>% select(`Date Time`,
+# data = data |> select(`Date Time`,
 #                 `T (degC)`,
 #                 `rh (%)`)
 # 
 # # daily
-# data_daily = data %>%
-#   mutate(Date = format(as.Date(dmy_hms(`Date Time`)), "%Y-%m-%d")) %>%
-#   group_by(Date) %>%
+# data_daily = data |>
+#   mutate(Date = format(as.Date(dmy_hms(`Date Time`)), "%Y-%m-%d")) |>
+#   filter(year(Date) >= 2014) |>
+#   group_by(Date) |>
 #   summarise(Temperature = mean(`T (degC)`),
 #             RelativeHumidity = mean(`rh (%)`))
 # 
-# # monthly 
-# data_monthly = data %>%
-#   mutate(Date = format(as.Date(dmy_hms(`Date Time`)), "%Y-%m")) %>%
-#   group_by(Date) %>%
+# # monthly
+# data_monthly = data |>
+#   mutate(Date = format(as.Date(dmy_hms(`Date Time`)), "%Y-%m")) |>
+#   group_by(Date) |>
 #   summarise(Temperature = mean(`T (degC)`),
-#             RelativeHumidity = mean(`rh (%)`))
+#             RelativeHumidity = mean(`rh (%)`)) |>
+#   filter(year(yearmonth(data_monthly$Date)) %in% c(2014, 2015, 2016))
 # 
 # readr::write_csv(as.data.frame(data_daily), file = "max_planck_weather_daily.csv")
 # readr::write_csv(as.data.frame(data_monthly), file = "max_planck_weather_monthly.csv")
 
-# importando dados
+
+# importando e tratando dados
+# queremos somente os dados a partir de 2014
 weather = read_csv("https://raw.githubusercontent.com/gabriel-alvaro/ME607-1S2023/main/models_EDA/max_planck_weather_daily.csv")
-weather = as_tsibble(weather)
+weather = as_tsibble(weather) |>
+  filter(year(Date) >= 2014)
+
+scan_gaps(weather)
+
+# adiciona dados faltantes (2016-10-26; 2016-10-27) com a media das colunas
+weather = weather |> fill_gaps(Temperature = mean(Temperature),
+                               RelativeHumidity = mean(RelativeHumidity))
 
 autoplot(weather, .vars = Temperature) +
   xlab("Date") +
   ylab("Temperature (Celsius)") +
+  ggtitle("Temperature (°C) in Jena, Germany from 2014 to 2016") +
   theme_bw() +
   scale_y_continuous(n.breaks = 6) +
   scale_x_yearmonth(date_breaks = "1 year",
@@ -47,6 +59,7 @@ autoplot(weather, .vars = Temperature) +
 ## MODELOS
 ## media, naive, naive sazonal, drift, regressao linear, suavizacao exponencial
 
+## sampling
 weather_train_rw = weather |>
   tile_tsibble(.size = 50)
 
@@ -54,14 +67,21 @@ weather_train_sw = weather |>
   stretch_tsibble(.step = 1, .init = 50)
 
 weather_train_rw |>
-  model(mean = MEAN(Temperature)) |>
+  model(mean = MEAN(Temperature),
+        naive = NAIVE(Temperature),
+        snaive = SNAIVE(Temperature ~ lag(12)),
+        drift = RW(Temperature ~ drift())) |>
   forecast(h = 1) |>
   accuracy(weather)
 
 weather_train_sw |>
-  model(mean = MEAN(Temperature)) |>
+  model(mean = MEAN(Temperature),
+        naive = NAIVE(Temperature),
+        snaive = SNAIVE(Temperature ~ lag(12)),
+        drift = RW(Temperature ~ drift())) |>
   forecast(h = 1) |>
   accuracy(weather)
+
 
 
 
